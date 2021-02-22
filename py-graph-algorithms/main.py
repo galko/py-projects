2bf484b710a4247dce6e4274f0d5633983896f70
galko <57600251+galko@users.noreply.github.com>
2021-02-22 19:31:01 +0200

    Update main.py

--- py-graph-algorithms/main.py
+++ py-graph-algorithms/main.py
@@ -3,6 +3,7 @@ from Graph import Graph
 
 my_graph = Graph("dir/data.txt", sep=',', is_directed=True)
 
+# Select any method.
 # my_graph.dijkstra_search(start=8, finish=7)
 # my_graph.prim_search(4)
 # my_graph.search_in_width(0)
@@ -10,6 +11,6 @@ my_graph = Graph("dir/data.txt", sep=',', is_directed=True)
 # my_graph.a_star(7, 8)
 # my_graph.ford_falkerson(0, 6)
 # my_graph.bidirectional_dijkstra(1, 2)
-my_graph.bidirectional_a_star(7, 4)
+# my_graph.bidirectional_a_star(7, 4)
 
 my_graph.draw()
